07c86510a0914f810c48ec19a2e565a8a2979cce
Hernandez Mejia Zahid <105386675+HernandezMejiaZahid@users.noreply.github.com>
2022-07-12 00:14:34 -0500

    Add files via upload

---
+++ DB/consultas.php
@@ -0,0 +1,21 @@
+<?php
+/* Mandamos a llamar  nuestra conexion de php  con require */
+require('./conexion.php');
+/* Declaramos una variable y dentro de ella asignamos una consulta */
+  $sql = "SELECT * FROM `cursos`;";
+
+  $result = mysqli_query($conexion, $sql);
+  
+  $diplomas = array();
+  while ($fila = mysqli_fetch_array($result)) {
+    array_push($diplomas, $fila);
+  }
+
+  echo json_encode($diplomas);
+
+  mysqli_free_result($result);
+
+  mysqli_close($conexion);  
+
+
+?>
